c644eb99b387c5a044735ee6e1422c1337c0b63a
itsmeumaima <umaimarauf2005@gmail.com>
2025-08-03 18:29:21 +0500

    added

--- weatherProject/myapp/views.py
+++ weatherProject/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import requests
+import datetime
 
 # Create your views here.
 def index(request):
+    # if 'city' in request.POST:
+    #     city=request.POST['city']
+    # else:
+    #     city='karachi'
+    # url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=d0891e9b7fbf7337ce84ebc6f2373023"
+    # PARAMS={'units':'metric'}
     return render(request,'index.html')
